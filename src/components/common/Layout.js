4f73228839fbe01d46afa8568961ac005f97b6d1
jayknyn <hack@jamminglobal.com>
2019-04-07 18:57:06 -0500

    updated cover image

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -53,7 +53,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                         </div>
                         { isHome ?
                             <div className="site-banner">
-                                <img className="site-banner-image" src={site.image} alt={site.title} />
+                                <img className="site-banner-image" src={site.cover_image} alt={site.title} />
                                 <h1 className="site-banner-title">{site.title}</h1>
                                 <p className="site-banner-desc">{site.description}</p>
                             </div> :
